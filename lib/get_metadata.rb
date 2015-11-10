e8131bee70dd7596a5a2c4c076f373c92495846c
Brady Wied <brady@bswtechconsulting.com>
2015-11-10 11:45:28 -0700

    Refactor to include physical + logical paths

--- lib/get_metadata.rb
+++ lib/get_metadata.rb
@@ -5,26 +5,31 @@ sprockets_env = Opal::RSpec::SprocketsEnvironment.new(spec_pattern=ARGV[0])
 Opal.paths.each { |p| sprockets_env.append_path p }
 sprockets_env.add_spec_paths_to_sprockets
 
-GET_FULL_PATHS = %w{opal opal-rspec}
-
-opal_rspec_paths = GET_FULL_PATHS.map { |p| sprockets_env[p].filename }
 pre_run_locator = Opal::RSpec::PreRackLocator.new sprockets_env.spec_pattern,
                                                   sprockets_env.spec_exclude_pattern,
                                                   sprockets_env.spec_files
 locator = Opal::RSpec::PostRackLocator.new(pre_run_locator)
 
-with_dependencies = locator.get_opal_spec_requires.map do |r|
-  main_asset = sprockets_env[r]
-  requires = main_asset.included.map { |dep| sprockets_env[dep].filename }
-  requires + [main_asset.filename]
-end.flatten
-
-File.open ARGV[1], 'w' do |test_req_file|
-  test_req_file << "Opal.require('opal/rspec');"
+map_assets = lambda do |assets, include_deps=true|
+  assets.map do |asset|
+    main_asset = sprockets_env[asset]
+    with_deps = [main_asset]
+    with_deps += main_asset.included.map { |dep| sprockets_env[dep] } if include_deps
+    with_deps.map do |dep_asset|
+      {
+          filename: dep_asset.filename,
+          logical_path: dep_asset.logical_path
+      }
+    end
+  end.flatten
 end
 
-watch = lambda { |watch_it, arr| [*arr].map { |a| {file: a, watch: watch_it} } }
+GET_FULL_PATHS = %w{opal opal-rspec}
+opal_rspec_paths = map_assets[GET_FULL_PATHS, include_deps=false]
+with_dependencies = map_assets[locator.get_opal_spec_requires]
 
-result = watch[false, opal_rspec_paths] + watch[false, ARGV[1]] + watch[true, with_dependencies]
+watch = lambda { |watch_it, arr| [*arr].map { |a| a.merge(watch: watch_it) } }
+
+result = watch[false, opal_rspec_paths] + watch[true, with_dependencies]
 
 puts result.to_json
