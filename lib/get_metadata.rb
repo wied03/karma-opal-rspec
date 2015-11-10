afe3263832d895091116b26a174ccc0885660e4f
Brady Wied <brady@bswtechconsulting.com>
2015-11-10 10:24:12 -0700

    Catch all sprockets files in here, not just rb ones

--- lib/get_metadata.rb
+++ lib/get_metadata.rb
@@ -14,21 +14,17 @@ pre_run_locator = Opal::RSpec::PreRackLocator.new sprockets_env.spec_pattern,
 locator = Opal::RSpec::PostRackLocator.new(pre_run_locator)
 
 with_dependencies = locator.get_opal_spec_requires.map do |r|
-  compiler = Opal::Compiler.new(File.read(r))
-  compiler.compile
-  compiler.requires + [r]
+  main_asset = sprockets_env[r]
+  requires = main_asset.included.map { |dep| sprockets_env[dep].filename }
+  requires + [main_asset.filename]
 end.flatten
-
-test_paths = with_dependencies.map do |r|
-  sprockets_env[r].filename
-end
 
 File.open ARGV[1], 'w' do |test_req_file|
   test_req_file << "Opal.require('opal/rspec');"
 end
 
-watch = lambda {|watch_it, arr| [*arr].map {|a| {file: a, watch: watch_it}}}
+watch = lambda { |watch_it, arr| [*arr].map { |a| {file: a, watch: watch_it} } }
 
-result = watch[false, opal_rspec_paths] + watch[false, ARGV[1]] + watch[true, test_paths]
+result = watch[false, opal_rspec_paths] + watch[false, ARGV[1]] + watch[true, with_dependencies]
 
 puts result.to_json
