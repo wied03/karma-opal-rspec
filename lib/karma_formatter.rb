7852713582766ec0b84791f413e379043bceb32a
Brady Wied <brady@bswtechconsulting.com>
2015-11-09 22:55:31 -0700

    fix formatter result issues

--- lib/karma_formatter.rb
+++ lib/karma_formatter.rb
@@ -3,67 +3,68 @@ require 'native'
 
 class Opal::RSpec::KarmaFormatter
   ::RSpec::Core::Formatters.register self, :start,
-                              :example_started,
-                              :example_passed, 
-                              :example_failed,
-                              :example_pending,
-                              :dump_summary
-                              
+                                     :example_started,
+                                     :example_passed,
+                                     :example_failed,
+                                     :example_pending,
+                                     :dump_summary
+
   def initialize(*args)
     super
     @karma = self.class.get_karma_instance
     @id = 0
   end
-                              
+
   def self.set_karma_instance(karma)
     @karma = karma
   end
-  
+
   def self.get_karma_instance
     @karma
   end
-  
+
   def start(notification)
     contents = {
-      total: notification.count
+        total: notification.count
     }
     `#{@karma}.info(#{contents.to_n})`
   end
-  
+
   def dump_summary(notification)
     `#{@karma}.complete()`
   end
-  
+
   def example_passed(notification)
     report_example_done notification, false, true
   end
-  
+
   def example_failed(notification)
     report_example_done notification, false, false
   end
-  
+
   def example_pending(notification)
     report_example_done notification, true, true
   end
-  
+
   def example_started(notification)
     @start_time = `new Date().getTime()`
   end
-  
+
   def report_example_done(notification, skipped, success)
     example = notification.example
+    suite = example.example_group.parent_groups.reverse.map { |grp| grp.description }
     result = {
-      description: example.description,
-      id: @id += 1,
-      log: success ? [] : notification.formatted_backtrace.split("\n").to_a,
-      skipped: skipped,
-      success: success,
-      suite: [],
-      time: skipped ? 0 : `new Date().getTime() - #{@start_time}`
+        description: example.description,
+        id: @id += 1,
+        log: success ? [] : notification.formatted_backtrace,
+        skipped: skipped,
+        success: success,
+        suite: suite,
+        time: skipped ? 0 : `new Date().getTime() - #{@start_time}`
     }
     `#{@karma}.result(#{result.to_n})`
-  end  
-  
+  end
+
   # TODO: Register for what Karma supports, then call Karma using opal's native helpers
 end
 
