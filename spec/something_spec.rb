3ef070c7bb4c19138847ad1b3ea4e1da969c6ef9
Brady Wied <brady@bswtechconsulting.com>
2015-11-10 00:42:04 -0700

    cleanup

--- spec/something_spec.rb
+++ spec/something_spec.rb
@@ -1,7 +1,9 @@
 require 'spec_helper'
 
 describe 'something' do
-  subject { 42 }
-  
-  it { is_expected.to eq 42 }
+  subject { 43 }
+
+  context 'nested' do
+    it { is_expected.to eq 42 }
+  end
 end
