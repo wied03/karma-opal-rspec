be326080274e8a36652abe1be99f74240dd6f8a4
Brady Wied <brady@bswtechconsulting.com>
2015-11-10 12:04:58 -0700

    Make more metadata adjustments, use hash keys to make lookup faster in preprocessor

--- lib/get_metadata.rb
+++ lib/get_metadata.rb
@@ -4,32 +4,37 @@ require 'json'
 sprockets_env = Opal::RSpec::SprocketsEnvironment.new(spec_pattern=ARGV[0])
 Opal.paths.each { |p| sprockets_env.append_path p }
 sprockets_env.add_spec_paths_to_sprockets
+sprockets_env.append_path File.dirname(__FILE__)
 
 pre_run_locator = Opal::RSpec::PreRackLocator.new sprockets_env.spec_pattern,
                                                   sprockets_env.spec_exclude_pattern,
                                                   sprockets_env.spec_files
 locator = Opal::RSpec::PostRackLocator.new(pre_run_locator)
 
-map_assets = lambda do |assets, include_deps=true|
-  assets.map do |asset|
+map_assets = lambda do |assets, watch, include_deps=true|
+  assets.inject({}) do |result, asset|
     main_asset = sprockets_env[asset]
     with_deps = [main_asset]
     with_deps += main_asset.included.map { |dep| sprockets_env[dep] } if include_deps
-    with_deps.map do |dep_asset|
-      {
-          filename: dep_asset.filename,
-          logical_path: dep_asset.logical_path
-      }
+    as_array = with_deps.map do |dep_asset|
+      [
+          dep_asset.filename,
+          {
+              logical_path: dep_asset.logical_path,
+              watch: watch
+          }
+      ]
     end
-  end.flatten
+    result.merge! Hash[as_array]
+  end
 end
 
 GET_FULL_PATHS = %w{opal opal-rspec}
-opal_rspec_paths = map_assets[GET_FULL_PATHS, include_deps=false]
-with_dependencies = map_assets[locator.get_opal_spec_requires]
+INTERNAL_PATHS = %w{karma_formatter}
+opal_rspec_paths = map_assets[GET_FULL_PATHS, watch=false, include_deps=false]
+internal_paths = map_assets[INTERNAL_PATHS, watch=false, include_deps=false]
+with_dependencies = map_assets[locator.get_opal_spec_requires, watch=true]
 
-watch = lambda { |watch_it, arr| [*arr].map { |a| a.merge(watch: watch_it) } }
-
-result = watch[false, opal_rspec_paths] + watch[true, with_dependencies]
+result = opal_rspec_paths.merge(internal_paths).merge(with_dependencies)
 
 puts result.to_json
