60df8544780f4b82f030c1256b0381341e99ffac
Brady Wied <brady@bswtechconsulting.com>
2015-11-11 15:24:13 -0700

    Fold middleware declaration in

--- karma.conf.js
+++ karma.conf.js
@@ -16,12 +16,8 @@ module.exports = function (config) {
             'spec/**/*_spec.rb'
         ],
 
-
         // list of files to exclude
         exclude: [],
-
-        // TODO: Move this inside the plugin
-        middleware: ['opal_sourcemap'],
 
         // preprocess matching files before serving them to the browser
         // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
