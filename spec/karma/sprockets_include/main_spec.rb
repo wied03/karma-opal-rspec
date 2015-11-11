953cd48847cfc9b0f1b125c1d01f685e459f1fc8
Brady Wied <brady@bswtechconsulting.com>
2015-11-11 11:28:37 -0700

    Organize tests into karma directories

--- spec/karma/sprockets_include/main_spec.rb
+++ spec/karma/sprockets_include/main_spec.rb
@@ -1,14 +1,7 @@
-require 'spec_helper'
 require 'sprockets_style'
 require 'via_sprockets'
 
 describe 'something' do
-  subject { 42 }
-
-  context 'nested' do
-    it { is_expected.to eq 42 }
-  end
-
   context 'via sprockets' do
     subject { ViaSprockets.stuff }
 
