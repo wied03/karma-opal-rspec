37ad59c8a127f32cdf848909c35161e230040a1a
Brady Wied <brady@bswtechconsulting.com>
2015-11-11 23:53:18 -0700

    Travis might be setting this variable so baseline this first

--- features/support/env.rb
+++ features/support/env.rb
@@ -7,6 +7,7 @@ Before do
   FileUtils.ln_s current_nm_dir, File.join(aruba.config.working_directory, 'node_modules')
   aruba.config.exit_timeout = 60
   aruba.config.io_wait_timeout = 60
+  set_environment_variable 'RAILS_ENV', nil
 end
 
 After do |scenario|
