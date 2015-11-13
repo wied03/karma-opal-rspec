129113de33796621a80a72f9b23fb9eced40ac66
Brady Wied <brady@bswtechconsulting.com>
2015-11-13 10:59:34 -0700

    sprockets deprecated the direct included accessor

--- lib/sprockets_metadata.rb
+++ lib/sprockets_metadata.rb
@@ -4,7 +4,7 @@ module SprocketsMetadata
   def self.get_dependency_graph(sprockets_env, files)
     files.map do |file_asset|
       asset = file_asset.is_a?(Sprockets::Asset) ? file_asset : sprockets_env.find_asset(file_asset)
-      raw_deps = (asset.included || []).map { |a| sprockets_env.find_asset(a) }
+      raw_deps = (asset.metadata[:included] || []).map { |a| sprockets_env.find_asset(a) }
                      .reject { |a| a.filename == asset.filename } # don't want ourself in here, sprockets includes that
       dependencies = get_dependency_graph sprockets_env, raw_deps
       Asset.new(asset.filename,
