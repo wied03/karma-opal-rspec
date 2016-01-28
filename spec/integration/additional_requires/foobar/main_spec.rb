9b6f81b5302062f9b0254bdefc8c36bc0349ef92
Brady Wied <brady@bswtechconsulting.com>
2016-01-27 17:08:58 -0700

    Placate rubocop

--- spec/integration/additional_requires/foobar/main_spec.rb
+++ spec/integration/additional_requires/foobar/main_spec.rb
@@ -1,7 +1,10 @@
 require 'browser'
 
 describe 'document' do
+  # it's not our global var
+  # rubocop:disable Style/GlobalVars
   subject { $document }
+  # rubocop:enable Style/GlobalVars
 
   it { is_expected.to respond_to :body }
 end
