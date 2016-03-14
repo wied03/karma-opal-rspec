0c745dc481ea9e58cf050f8ee95bd759cdc29ddf
Brady Wied <brady@bswtechconsulting.com>
2016-03-13 22:08:02 -0600

    Fix the our own metadata stuff

--- spec/unit/mri/metadata_server_spec.rb
+++ spec/unit/mri/metadata_server_spec.rb
@@ -27,6 +27,7 @@ describe Karma::Opal::MetadataServer do
   let(:default_roll_up_list) { [] }
 
   before do
+    # TODO: Remove exclude_self, derive watch from whether it's a GEM
     contents = {
       files: [*requested_files],
       watch: watch,
