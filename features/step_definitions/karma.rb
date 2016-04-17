e20289a9ddf86edbc60ac950a8db774f6c628e57
Brady Wied <brady@bswtechconsulting.com>
2016-04-17 17:55:04 -0600

    Don't need to test these cases now, that's all webpack and karma

--- features/step_definitions/karma.rb
+++ features/step_definitions/karma.rb
@@ -77,56 +77,6 @@ Given(/^I copy (\S+) to the working directory$/) do |path|
   `cp -R #{path} #{dest}`
 end
 
-BASE_URL = 'http://localhost:9876'.freeze
-
-And(/^the following source maps exist:$/) do |expected_maps|
-  expected_maps.hashes.each do |expected|
-    expected_source_map_path = expected[:'Map URL']
-    js_url = URI.join(BASE_URL, expected[:File])
-    open(js_url) do |js_file|
-      expect(js_file.read).to include "//# sourceMappingURL=#{expected_source_map_path}"
-    end
-    source_map_full_path = File.expand_path("../#{expected_source_map_path}", js_url.path)
-    source_map_contents = nil
-    open(URI.join(BASE_URL, source_map_full_path)) do |source_map|
-      source_map_contents = source_map.read
-    end
-    source_map_contents = JSON.parse source_map_contents
-    expect(source_map_contents['file']).to eq expected[:'Original File']
-    expected_sources = expected[:Sources].split ','
-    expect(source_map_contents['sources']).to eq expected_sources
-    expected_sources.each do |source|
-      open(URI.join(BASE_URL, source)) do |original_source|
-        expect(original_source.read).to_not be_empty
-      end
-    end
-  end
-end
-
-And(/^the following files do not have source maps:$/) do |table|
-  # table is a table.hashes.keys # => [:File]
-  table.hashes.each do |file|
-    open(URI.join(BASE_URL, file[:File])) do |js_file|
-      expect(js_file.read).to_not include 'sourceMappingURL'
-    end
-  end
-end
-
-And(/^the following files have unresolvable source maps:$/) do |table|
-  # table is a table.hashes.keys # => [:File]
-  table.hashes.each do |file|
-    source_map_path = nil
-    js_url = URI.join(BASE_URL, file[:File])
-    open(js_url) do |js_file|
-      match = %r{//# sourceMappingURL=(.*)}.match(js_file.read)
-      expect(match).to_not be_nil
-      source_map_path = match.captures[0]
-    end
-    source_map_full_path = File.expand_path("../#{source_map_path}", js_url.path)
-    expect { open(URI.join(BASE_URL, source_map_full_path)) { |f| } }.to raise_exception OpenURI::HTTPError, '404 Not Found'
-  end
-end
-
 Then(/^the test fails$/) do
   step 'the exit status should not be 0'
 end
